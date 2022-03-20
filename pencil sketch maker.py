067d86dff19e9b1edeae60152647a0cecaf3b48f
Kishormt <101721762+Kishormt@users.noreply.github.com>
2022-03-20 10:19:29 +0530

    Add files via upload

---
+++ pencil sketch maker.py
@@ -0,0 +1,87 @@
+import cv2
+import matplotlib.pyplot as plt
+
+img=cv2.imread("mickey.jpg")
+
+cv2.imshow('original image',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+plt.imshow(img)
+plt.axis(False)
+plt.savefig('temp.png')
+plt.show()
+
+plt.imshow(img[:,:,::-1])
+plt.axis(False)
+plt.savefig('temp.png')
+plt.show()
+
+RGB_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+plt.imshow(RGB_img)
+plt.axis(False)
+plt.savefig('temp.png')
+plt.show()
+
+grey_img=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+invert_img=cv2.bitwise_not(grey_img)
+
+blur_img=cv2.GaussianBlur(invert_img, (111,111),0)
+
+invblur_img=cv2.bitwise_not(blur_img)
+
+sketch_img=cv2.divide(grey_img,invblur_img, scale=256.0)
+
+cv2.imwrite('sketch.png', sketch_img)
+
+cv2.imshow('sketch image',sketch_img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+plt.figure(figsize=(14,8))
+
+plt.subplot(1,2,1)
+plt.title('Original image', size=18)
+plt.imshow(RGB_img)
+plt.axis('off')
+
+plt.subplot(1,2,2)
+plt.title('Sketch', size=18)
+rgb_sketch=cv2.cvtColor(sketch_img, cv2.COLOR_BGR2RGB)
+plt.imshow(rgb_sketch)
+plt.axis('off')
+plt.savefig('temp1.png')
+plt.show()
+
+def sketch_image(photo, k_size):
+   
+    img=cv2.imread(photo)
+    
+   
+    grey_img=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    
+    invert_img=cv2.bitwise_not(grey_img)
+    
+
+   
+    blur_img=cv2.GaussianBlur(invert_img, (k_size,k_size),0)
+
+    
+    invblur_img=cv2.bitwise_not(blur_img)
+    
+
+    
+    sketch_img=cv2.divide(grey_img,invblur_img, scale=256.0)
+
+    
+    cv2.imwrite('sketch.png', sketch_img)
+
+    
+    cv2.imshow('sketch image',sketch_img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    
+
+sketch_image(photo='image_1.png', k_size=7)
